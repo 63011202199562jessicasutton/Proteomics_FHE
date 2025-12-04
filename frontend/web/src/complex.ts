135145146df96ea3b9a4eb542d00e05ca4b32ece
63011202199562jessicasutton <oksanagelira@mail.ru>
2025-12-04 13:47:59 +0800

    add serializer.py, complex.ts, IDecryptionOracle__factory.ts

---
+++ frontend/web/src/complex.ts
@@ -0,0 +1,3 @@
+import { ethers } from 'ethers';
+
+export default function environment-common() { return <div>Component</div>; }
---
+++ frontend/web/src/serializer.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\serializer.py
+// Generated at: 2025-10-11T02:59:28.929Z
